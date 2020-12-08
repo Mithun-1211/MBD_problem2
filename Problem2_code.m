74c77e150e0c3d39ca9ad264a15989efde45d44f
Mithun <mithunsharaff96@gmail.com>
2020-12-08 13:23:17 +0530

    Changed the dummy code

--- Problem2_code.m
+++ Problem2_code.m
@@ -1,14 +1,12 @@
-sinbump = @(L,A) A*(0.5+0.5*(sin(linspace(-pi/2,pi*3/2,25*L))));
-B1 = sinbump(1, 1);
-B2 = sinbump(5, 0.5);
-figure(1)
-subplot(2,1,1)
-plot(B1)
-grid
-title('Amplitude = 1.  Length = 25')
-axis([0  150    0  1.5])
-subplot(2,1,2)
-plot(B2)
-grid
-title('Amplitude = 0.5.  Length = 125')
-axis([0  150    0  1.5])
+th = linspace(0,3*pi) ;
+r=0.1*sin(th) ; 
+if (th>pi & th<1.5*pi)
+    r=0;
+end
+if th > 1.5*pi
+    r=0.05*sin(th);
+end
+th=th';
+r=r';
+plot(th,r,'linewidth',10);
+sim('problem_2');
